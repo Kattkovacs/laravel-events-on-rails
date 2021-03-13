a495bdbd0eca5cf2a99e3ec778ea66e0e5e052d1
Kattkovacs <kattkovacs@gmail.com>
2021-03-13 19:17:18 +0100

    add route parameter $id as wildcard

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,8 @@ Route::get('/trains', function () {
         'age' => request('age')
     ]);
 });
+
+Route::get('/trains/{id}', function ($id) {
+    // use the $id variable to query the db for a record
+    return view('details', ['id' => $id]);
+});
